656acac9a2ab500c15c86fe91aece242365b1552
Philip Jordan <DigitalMoss@live.com>
2014-05-17 11:05:15 -0400

    Kata code for Brett

---
+++ C#/MarsRoverKata/MarsRoverKata/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRoverKata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mars = new Mars();
+            var rover = new Rover(mars);
+            var mission = new MissionManager(rover);
+            CaptureObsticles(mars);
+            PrintRoverInstructions();
+            Console.WriteLine("Mars Rover Mission Control!");
+
+            var input = string.Empty;
+            while (!input.Equals("q", StringComparison.CurrentCultureIgnoreCase))
+            {
+                if (input.Equals("h", StringComparison.CurrentCultureIgnoreCase))
+                {
+                    PrintRoverInstructions();
+                }
+                else
+                {
+                    IssueCommands(mission, input);
+                    OutputMissionStatus(rover);
+                }
+                
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine("Mission complete! Congrats!");
+            Console.ReadLine();
+        }
+
+        private static void CaptureObsticles(Mars mars)
+        {
+            Console.WriteLine("Create Mars!");
+            Console.WriteLine("Enter obsticles to the Mars landscape in X,Y format.");
+            Console.WriteLine("Press [Enter] with no values when done.");
+            string input = Console.ReadLine();
+            while (!string.IsNullOrWhiteSpace(input))
+            {
+                var coordinates = input.Split(',');
+                Point location = new Point(int.Parse(coordinates[0]), int.Parse(coordinates[1]));
+                Obsticle obsticle = new Obsticle(location);
+                mars.Accept(obsticle);
+                input = Console.ReadLine();
+            }
+            Console.WriteLine();
+        }
+
+        private static void IssueCommands(MissionManager mission, string input)
+        {
+            string commandsAcceptance = mission.AcceptCommands(input);
+            if (commandsAcceptance != String.Empty)
+            {
+                Console.WriteLine(commandsAcceptance);
+            }
+
+            string execution = mission.ExecuteMission();
+            if (execution != String.Empty)
+            {
+                Console.WriteLine(execution);
+            }
+        }
+
+        private static void OutputMissionStatus(Rover rover)
+        {
+            var roverLocation = rover.Location;
+            Console.WriteLine("The rover is now at X: " + roverLocation.X + " Y: " + roverLocation.Y);
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Enter more commands to continue moving: ");
+        }
+
+        private static void PrintRoverInstructions()
+        {
+            Console.WriteLine("Commands:");
+            Console.WriteLine("f - Move Forward");
+            Console.WriteLine("b - Move Backward");
+            Console.WriteLine("r - Turn Right");
+            Console.WriteLine("l - Turn Left");
+            Console.WriteLine("q - End Mission (Single character command)");
+            Console.WriteLine("h - Display Help (Single character command)");
+            Console.WriteLine("You may issue multiple rover commands on one line.");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ C#/MarsRoverKata/MarsRoverKata/Rover.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MarsRoverKata.Commands;
+
+namespace MarsRoverKata
+{
+    //public enum Direction
+    //{
+    //    North, 
+    //    East,
+    //    South,
+    //    West
+    //}
+
+    //public class CrashException : Exception
+    //{
+    //    public CrashException(String message) : base(message)
+    //    { }
+    //}
+
+    public class Rover
+    {
+        public Point Location { get; set; }
+        public Direction Facing { get; set; }
+
+        private Mars Mars { get; set; }
+        private Dictionary<Direction, Point> PositionalAdjustments;
+
+        public Rover(Mars mars)
+            :this(mars, new Point(0,0))
+        { }
+
+        public Rover(Mars mars, Point landingPoint)
+        {
+            Mars = mars;
+            Location = landingPoint;
+
+            if (!Mars.IsValidPosition(Location))
+                throw new CrashException("Doh! We tried to land on something other than the planet and the rover was destroyed!!!");
+
+            Facing = Direction.North;
+            PositionalAdjustments = new Dictionary<Direction, Point>() 
+            { 
+                { Direction.North, new Point(0, 1) },
+                { Direction.South, new Point(0, -1) },
+                { Direction.East, new Point(1, 0) },
+                { Direction.West, new Point(-1, 0) }
+            };
+        }
+
+        public bool MoveBackward()
+        {
+            return Move(-1);
+        }
+
+        public bool MoveForward()
+        {
+            return Move(1);
+        }
+
+        private bool Move(int adjustmentFactor)
+        {
+            var adjustment = PositionalAdjustments[Facing] * adjustmentFactor;
+            var desiredPosition = Location + adjustment;
+            var newLocation = Mars.CalculateFinalPosition(Location, desiredPosition);
+
+            var success = Location == newLocation ? false : true;
+
+            Location = newLocation;
+
+            return success;
+        }
+
+        public bool TurnRight()
+        {
+            if (Facing == Direction.North)
+                Facing = Direction.East;
+            else if (Facing == Direction.South)
+                Facing = Direction.West;
+            else if (Facing == Direction.East)
+                Facing = Direction.South;
+            else if (Facing == Direction.West)
+                Facing = Direction.North;
+
+            return true;
+        }
+
+        public bool TurnLeft()
+        {
+            if (Facing == Direction.North)
+                Facing = Direction.West;
+            else if (Facing == Direction.South)
+                Facing = Direction.East;
+            else if (Facing == Direction.East)
+                Facing = Direction.North;
+            else if (Facing == Direction.West)
+                Facing = Direction.South;
+            
+            return true;
+        }
+    }
+}
