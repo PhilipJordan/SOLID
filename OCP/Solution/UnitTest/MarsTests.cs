a24ca1aa31869beb33e19160059803733c29d217
Philip Jordan <philip.jordan@live.com>
2014-07-30 10:05:02 -0400

    trying to get unit tests to appear in test explorer

--- OCP/Solution/UnitTest/MarsTests.cs
+++ OCP/Solution/UnitTest/MarsTests.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
-using NUnit.Core;
+//using NUnit.Core;
 using NUnit;
 using MarsRoverKata;
 using MarsRoverKata.Commands;
