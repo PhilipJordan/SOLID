888671f4b6bf12694e834967d4bfc73045767d85
bojingo <bojingo@yahoo.com>
2014-09-21 11:44:21 -0400

    More or less got the aliens moving around.  Still plenty to do - like killing the rover if an alien hits it!

--- LSP+ISP/Spoilers/MarsRoverKata/Alien.cs
+++ LSP+ISP/Spoilers/MarsRoverKata/Alien.cs
@@ -6,10 +6,79 @@ using System.Threading.Tasks;
 
 namespace MarsRoverKata
 {
-    public class Alien : Obstacle
+    public class Alien : Obstacle, IMovable
     {
-        public Alien(Point location)
+        public Direction Facing { get; set; }
+
+        public Mars Mars { get; private set; }
+        private static readonly Dictionary<Direction, Point> PositionalAdjustments = new Dictionary<Direction, Point>
+        { 
+            { Direction.North, new Point(0, 1) },
+            { Direction.South, new Point(0, -1) },
+            { Direction.East, new Point(1, 0) },
+            { Direction.West, new Point(-1, 0) }
+        };
+
+        public Alien(Mars mars, Point location)
             : base(location)
-        { }
+        {
+            Mars = mars;
+        }
+
+        public bool MoveBackward()
+        {
+            return Move(-1);
+        }
+
+        public bool MoveForward()
+        {
+            return Move(1);
+        }
+
+        private bool Move(int adjustmentFactor)
+        {
+            var desiredPosition = CreateDesiredPosition(adjustmentFactor);
+            var newLocation = Mars.CalculateFinalPosition(Location, desiredPosition);
+
+            var success = Location != newLocation;
+
+            Location = newLocation;
+
+            return success;
+        }
+
+        private Point CreateDesiredPosition(int adjustmentFactor)
+        {
+            var adjustment = PositionalAdjustments[Facing] * adjustmentFactor;
+            return Location + adjustment;
+        }
+
+        public bool TurnRight()
+        {
+            if (Facing == Direction.North)
+                Facing = Direction.East;
+            else if (Facing == Direction.South)
+                Facing = Direction.West;
+            else if (Facing == Direction.East)
+                Facing = Direction.South;
+            else if (Facing == Direction.West)
+                Facing = Direction.North;
+
+            return true;
+        }
+
+        public bool TurnLeft()
+        {
+            if (Facing == Direction.North)
+                Facing = Direction.West;
+            else if (Facing == Direction.South)
+                Facing = Direction.East;
+            else if (Facing == Direction.East)
+                Facing = Direction.North;
+            else if (Facing == Direction.West)
+                Facing = Direction.South;
+
+            return true;
+        }
     }
 }
---
+++ LSP+ISP/Spoilers/MarsRoverKata/IMovable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRoverKata
+{
+    public interface IMovable
+    {
+        bool MoveBackward();
+        bool MoveForward();
+        bool TurnRight();
+        bool TurnLeft();
+    }
+}
