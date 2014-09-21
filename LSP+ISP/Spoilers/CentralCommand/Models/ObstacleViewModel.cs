a687745a7742f854075e3657782a9b9b1973769d
bojingo <bojingo@yahoo.com>
2014-09-21 11:00:15 -0400

    Added Alien as an obstacle type.  UI all updated and aliends can be destroyed by missles/mortars.  Now to make the alien move!

---
+++ LSP+ISP/Spoilers/CentralCommand/Models/ObstacleViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CentralCommand.Models
+{
+    public class ObstacleViewModel
+    {
+        public string Coordinates { get; set; }
+        public string Type { get; set; }
+    }
+}
---
+++ LSP+ISP/Spoilers/MarsRoverKata/Alien.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRoverKata
+{
+    public class Alien : Obstacle
+    {
+        public Alien(Point location)
+            : base(location)
+        { }
+    }
+}
