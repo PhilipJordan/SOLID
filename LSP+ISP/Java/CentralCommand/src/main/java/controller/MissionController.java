e0111b0f7e9291204cd04996bf81b7e079a19cae
J Alexander <jalex@pobox.com>
2014-11-30 23:19:17 -0500

    Folder Reorganization and dependency cleanup
    Reorganized folder structure to accommodate a more mirrored structure of
    the .Net code.  Unsure how to the frontend will integrate, once
    decoupled, but this is a start.
    
    Updated dependencies and libraries used by using the maven dependencies
    for
    org.glassfish.jersey.containers:jersey-container-grizzly2-http:2.13
    and
    org.glassfish.jersey.media:jersey-media-json-jackson:2.13

--- LSP+ISP/Java/CentralCommand/src/main/java/controller/MissionController.java
+++ LSP+ISP/Java/CentralCommand/src/main/java/controller/MissionController.java
@@ -7,8 +7,13 @@ import org.glassfish.jersey.server.ResourceConfig;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.GenericEntity;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.io.IOException;
 import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 // The Java class will be hosted at the URI path "/r"
@@ -19,22 +24,37 @@ public class MissionController {
 
     // The Java method will process HTTP GET requests
     @GET
-    // The Java method will produce content identified by the MIME Media type "text/plain"
-    @Produces("text/plain")
-    public String getClichedMessage() {
-        // Return some cliched textual content
-        return "Hello World";
+    // The Java method will produce content identified by a specified MIME Media type
+    @Produces(MediaType.APPLICATION_JSON)
+    // Path determines the path, which is /r/example for this case
+    @Path("/example")
+    public Response getExampleMessage() {
+        Map<String, Integer> testMap = new HashMap<String, Integer>();
+        testMap.put("One", 1);
+        testMap.put("Twelve", 12);
+        testMap.put("Three", 3);
+        GenericEntity<Map<String, Integer>> e = new GenericEntity<Map<String, Integer>>(testMap) {
+        };
+        return Response.ok(testMap).build();
     }
 
     @GET
-    // The Java method will be hosted at the URI path "/r/reset"
+    // The Java method will be hosted at the URI path "/r/reset"-
     @Path("/reset")
-    @Produces("text/plain")
+    @Produces(MediaType.APPLICATION_JSON)
     public String getReset() {
         // TODO: Reset
         return "Reset";
     }
 
+    @GET
+    @Path("/index")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getIndex() {
+        return null;
+    }
+
+    // MAIN - execution starting point
     public static void main(String[] args) throws IOException {
         final ResourceConfig resourceConfig = new ResourceConfig(MissionController.class);
         final HttpServer server = GrizzlyHttpServerFactory.createHttpServer(BASE_URI, resourceConfig);
@@ -42,8 +62,7 @@ public class MissionController {
         server.start();
 
         System.out.println("Server running");
-        System.out.println("Visit: http://localhost:" + PORT + "/r");
-        System.out.println("Or: http://localhost:" + PORT + "/r/reset");
+        System.out.println("URL (example): http://localhost:" + PORT + "/r/example");
         System.out.println("Hit return to stop...");
         System.in.read();
         System.out.println("Stopping server");
