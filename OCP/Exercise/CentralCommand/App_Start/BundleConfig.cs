8927be8eedb1f3edf4fd548e4fa300b0456c8441
bojingo <bojingo@yahoo.com>
2014-12-24 16:09:49 -0500

    Removed jqueryui nuget package since it wasn't being used.

--- OCP/Exercise/CentralCommand/App_Start/BundleConfig.cs
+++ OCP/Exercise/CentralCommand/App_Start/BundleConfig.cs
@@ -11,9 +11,6 @@ namespace CentralCommand
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                        "~/Scripts/jquery-ui-{version}.js"));
-
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.unobtrusive*",
                         "~/Scripts/jquery.validate*"));
