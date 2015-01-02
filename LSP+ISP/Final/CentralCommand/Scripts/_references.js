69e83aa8372aedfbfe19f0aa76b251a1d89a08d4
bojingo <bojingo@yahoo.com>
2015-01-01 20:46:14 -0500

    Removed unused validation NuGet packages.

--- LSP+ISP/Final/CentralCommand/Scripts/_references.js
+++ LSP+ISP/Final/CentralCommand/Scripts/_references.js
@@ -1,5 +1,2 @@
 ﻿/// <reference path="modernizr-2.6.2.js" />
 /// <reference path="jquery-1.9.0.js" />
-/// <reference path="jquery.validate.js" />
-/// <reference path="jquery.validate.unobtrusive.js" />
-/// <reference path="knockout-2.2.0.debug.js" />
