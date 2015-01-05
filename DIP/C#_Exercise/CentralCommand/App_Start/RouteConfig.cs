c846180fb5c29b9c7a83512a47d41c1fae71d578
bojingo <bojingo@yahoo.com>
2015-01-05 11:58:02 -0500

    Updated .NET code to use index.html as the start page instead of going to a controller index action.

--- DIP/C#_Exercise/CentralCommand/App_Start/RouteConfig.cs
+++ DIP/C#_Exercise/CentralCommand/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace CentralCommand
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Mission", action = "Index", id = UrlParameter.Optional }
+                defaults: new { action = "Index", id = UrlParameter.Optional }
             );
         }
     }
