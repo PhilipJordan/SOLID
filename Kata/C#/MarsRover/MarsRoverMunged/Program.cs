9d96381e939d5ec2ce081b17f0d6e3301fae06ed
bojingo <bojingo@yahoo.com>
2015-01-06 11:23:38 -0500

    Added Kata project.

---
+++ Kata/C#/MarsRover/MarsRoverMunged/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRoverMunged
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Everything all = new Everything();
+            all.Run();
+        }
+    }
+}
